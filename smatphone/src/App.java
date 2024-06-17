95972ceda02e5d35f6531f41fccbbffd17b33dbf
projetos <claytonsalesdev@gmail.com>
2024-06-17 11:39:34 -0300

    .
    .

--- smatphone/src/App.java
+++ smatphone/src/App.java
@@ -2,15 +2,15 @@ import smartphone.Iphone;
 
 public class App {
     public static void main(String[] args) {
-        Iphone quinze = new Iphone();
+        Iphone iphonedoisg = new Iphone();
 
-        quinze.adicionarNovaAba();
-        quinze.atualizarPagina();
-        quinze.atender();
-        quinze.iniciarCorreioVoz();
-        quinze.selecionarMusica(": musica");
-        quinze.tocar();
-        quinze.pausar();
+        iphonedoisg.adicionarNovaAba();
+        iphonedoisg.atualizarPagina();
+        iphonedoisg.atender();
+        iphonedoisg.iniciarCorreioVoz();
+        iphonedoisg.selecionarMusica(": musica");
+        iphonedoisg.tocar();
+        iphonedoisg.pausar();
         
     }
 }
